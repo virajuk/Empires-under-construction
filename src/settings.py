e523b93d94619b062c49a40c719b724cffdae04b
virajuk <uk.viraj@gmail.com>
2024-08-21 13:33:19 -0400

    minor updates

--- src/settings.py
+++ src/settings.py
@@ -2,7 +2,7 @@
 WIDTH = 1792
 HEIGHT = 960
 FPS = 60
-TILE_SIZE = 16
+TILE_SIZE = 32
 
 # WORLD_MAP = [
 # ['grass','grass','grass','grass','grass','grass','grass','grass','grass','grass','grass','grass','grass','grass','grass','grass','grass','grass','grass','grass'],
