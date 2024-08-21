2934e4ffdf52229f4dc26a32acc5a5057dd18fe4
viraj uk <virajk@saksglobal.com>
2024-08-21 12:11:08 -0400

    tree generation | tiled version

--- src/objects.py
+++ src/objects.py
@@ -24,7 +24,7 @@ class Objects:
         self.tree_sprites = pygame.sprite.Group()
 
         self.create_map()
-        self.create_trees()
+        self.plant_trees()
 
     def create_map(self):
 
@@ -44,47 +44,34 @@ class Objects:
                 # tile = random.choices([GreenGrass, Sand, Water], weights=[100, 1, 1], k=1)[0]
                 tile = random.choices([GreenGrass], weights=[100], k=1)[0]
                 tile((x, y), (self.visible_sprites, ), cell)
+
                 Grid((x, y), (self.visible_sprites,))
 
 
-        # for alien in self.visible_sprites.sprites():
-        #     print(alien.rect.center)
-
-    def create_trees(self):
+    def plant_trees(self):
 
         # noise = generate_fractal_noise_2d((settings.WIDTH, settings.HEIGHT), (8, 6), 5)
         # noise = generate_fractal_noise_2d((settings.WIDTH, settings.HEIGHT), (8, 6))
 
-        noise = generate_perlin_noise_2d((settings.WIDTH, settings.HEIGHT), (8, 6))
+        noise = generate_perlin_noise_2d((settings.WIDTH, settings.HEIGHT), (28, 15))
 
-        lower_bound, higher_bound = 0.4555, 0.456
+        lower_bound, higher_bound = 0.4941, 0.4949
 
         combined_condition = np.logical_and(noise >= lower_bound, noise <= higher_bound)
         indices = np.where(combined_condition)
 
         for location in zip(indices[0], indices[1]):
 
-            Trees((location[0], location[1]), (self.tree_sprites,))
+            x,y = (int(location[0]/settings.TILE_SIZE)*settings.TILE_SIZE), (int(location[1]/settings.TILE_SIZE)*settings.TILE_SIZE)
+            Trees((x, y), (self.tree_sprites,))
 
-        # patch_1 = (int(self.map_size[1]/4), int(self.map_size[0]/4))
-        # patch_2 = (int(3*self.map_size[1]/4), int(self.map_size[0]/4))
-        # patch_3 = (int(self.map_size[1]/4), int(3*self.map_size[0]/4))
-        # patch_4 = (int(3*self.map_size[1]/4), int(3*self.map_size[0]/4))
-
-        # patch_1 = (int(15), int(3))
-
-        # print(patch_1)
-
-        # Trees((patch_1[0]*settings.TILE_SIZE, patch_1[1]*settings.TILE_SIZE), (self.tree_sprites, ))
-        # Trees((patch_2[0]*settings.TILE_SIZE, patch_2[1]*settings.TILE_SIZE), (self.tree_sprites, ))
-        # Trees((patch_3[0]*settings.TILE_SIZE, patch_3[1]*settings.TILE_SIZE), (self.tree_sprites, ))
-        # Trees((patch_4[0]*settings.TILE_SIZE, patch_4[1]*settings.TILE_SIZE), (self.tree_sprites, ))
-
-        # print(self.tree_sprites)
-        # print(pygame.math.clamp(0, 2, 25))
-        # print(pygame.math.lerp(0, 50, 0.3))
 
     def run(self):
 
         self.visible_sprites.draw(self.display_surface)
         self.tree_sprites.draw(self.display_surface)
+
+        # for one in self.visible_sprites:
+        #
+        #     if isinstance(one,GreenGrass):
+        #         print(one.id)
--- src/settings.py
+++ src/settings.py
@@ -1,8 +1,8 @@
 # game setup
-WIDTH = 1280
+WIDTH = 1792
 HEIGHT = 960
 FPS = 60
-TILE_SIZE = 64
+TILE_SIZE = 16
 
 # WORLD_MAP = [
 # ['grass','grass','grass','grass','grass','grass','grass','grass','grass','grass','grass','grass','grass','grass','grass','grass','grass','grass','grass','grass'],
